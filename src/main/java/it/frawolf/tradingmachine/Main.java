a094f03366c8d112c1cb5b3c23802d614a7ac4c6
Fra <frawolf@outlook.it>
2020-12-15 12:05:47 +0100

    Started main package

---
+++ src/main/java/it/frawolf/tradingmachine/Main.java
@@ -0,0 +1,8 @@
+package it.frawolf.tradingmachine;
+
+import it.frawolf.tradingmachine.util.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class Main {
+
+}
