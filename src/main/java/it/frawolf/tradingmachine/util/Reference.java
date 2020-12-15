9371e7dcb54e8a4521d15018b1aa435b82fc4afa
Fra <frawolf@outlook.it>
2020-12-15 12:05:34 +0100

    Added reference file

---
+++ src/main/java/it/frawolf/tradingmachine/util/Reference.java
@@ -0,0 +1,12 @@
+package it.frawolf.tradingmachine.util;
+
+public class Reference {
+	
+	public static final String MOD_ID = "tm";
+	public static final String NAME = "Trading Machine Mod";
+	public static final String VERSION = "1.0.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+	
+}
